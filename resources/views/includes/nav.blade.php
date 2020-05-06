1f467bfa6a0bd0f3490418412ef18ce5106350da
Christopher Heyder <christopherheyder@mail.de>
2020-05-06 14:30:35 +0200

    Refine styling breadcrumb with icons

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,7 +1,7 @@
 @if(Request::is('*read*'))
     <a class="fixed-top w-auto" style="height:48px" href="{{ route('desk') }}"></a>
 @else
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm py-0">
 
         <div class="container">
             
@@ -10,13 +10,24 @@
             </a>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item">
-                    <a class="badge badge-pill badge-primary" href="#">.</a>
+                    <a class="btn p-1" href="">
+                        <img src="{{ asset('icons/archive-cabinet.png') }}" style="height:24px" alt="">
+                    </a>
                 </li>
                 <li class="breadcrumb-item">
-                    <a class="badge badge-pill badge-primary" href="#">.</a>
+                    <a class="btn p-1" href="">
+                        <img src="{{ asset('icons/archive-box.png') }}" style="height:24px" alt="">
+                    </a>
                 </li>
-                <li class="breadcrumb-item active" aria-current="page">
-                    <a href="#" class="badge badge-pill badge-primary">.</a>
+                <li class="breadcrumb-item">
+                    <a class="btn p-1" href="">
+                        <img src="{{ asset('icons/folder.png') }}" style="height:24px" alt="">
+                    </a>
+                </li>
+                <li class="breadcrumb-item">
+                    <a class="btn p-1" href="">
+                        <img src="{{ asset('icons/document.png') }}" style="height:24px" alt="">
+                    </a>
                 </li>
             </ol>
         </div>
