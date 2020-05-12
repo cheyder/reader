86155f2c91f6278232fb44594218aecf65d522f1
Christopher Heyder <christopherheyder@mail.de>
2020-05-12 15:24:22 +0200

    Create file (bugFix)

--- app/File.php
+++ app/File.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-  protected $fillable = ['title'];
+  protected $fillable = [
+    'title',
+    'url',
+    'position'
+  ];
   
   public function parent() {
       return $this->belongsTo('App\Folder', 'parent_id');
