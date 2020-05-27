699519cf80bc9d19e0c76a0f9e9a90d8f0e5731d
Christopher Heyder <christopherheyder@mail.de>
2020-05-27 15:33:21 +0200

    Test store file, delete files (bug), folders (bug)

--- tests/Feature/DeskTest.php
+++ tests/Feature/DeskTest.php
@@ -6,44 +6,135 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Folder;
+use App\File;
 use App\User;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rules\Exists;
 
 class DeskTest extends TestCase
 {
   use RefreshDatabase, WithFaker;
    
-    public function testStoreFolderAndSubfolder()
-    {
-      $folderInput = [
-        'type' => 'folder',
-        'title' => 'Testfolder' 
-      ];
-      $user = factory(User::class)->create();
+  public function testStoreFolderAndSubfolder()
+  {
+  $folderInput = [
+    'type' => 'folder',
+    'title' => 'Testfolder' 
+  ];
+  $user = factory(User::class)->create();
+  $currentFolder = $this->getRootFolder($user);
+  $this->actingAs($user)->post(route('desk.store', ['currentFolderId' => $currentFolder->id]), $folderInput);
+  $this->assertDatabaseHas('folders', [
+    'title' => 'Testfolder',
+    'parent_id' => $currentFolder->id,
+  ]);
 
-      $rootFolder = [
-        'user_id' => $user->id,
-        'title' => 'Desk of' . $user->name,
-        'parent_id' => NULL
-      ];
-      $currentFolder = Folder::create($rootFolder);
-
-      $this->actingAs($user)->post(route('desk.store', ['currentFolderId' => $currentFolder->id]), $folderInput);
-
-      $this->assertDatabaseHas('folders', [
-        'title' => 'Testfolder',
-        'parent_id' => $currentFolder->id,
-      ]);
-
-      $currentFolder = Folder::where('title', 'Testfolder')->first();
-      $subFolderInput = [
-        'type' => 'folder',
-        'title' => 'SubTestfolder'
-      ];
-      $this->actingAs($user)->post(route('desk.store', ['currentFolderId' => $currentFolder->id]), $subFolderInput);
-    
-      $this->assertDatabaseHas('folders', [
-          'title' => 'SubTestfolder',
-          'parent_id' => $currentFolder->id,
-        ]);
-      }
+  $currentFolder = Folder::where('title', 'Testfolder')->first();
+  $subFolderInput = [
+    'type' => 'folder',
+    'title' => 'SubTestfolder'
+  ];
+  $this->actingAs($user)->post(route('desk.store', ['currentFolderId' => $currentFolder->id]), $subFolderInput);
+  $this->assertDatabaseHas('folders', [
+      'title' => 'SubTestfolder',
+      'parent_id' => $currentFolder->id,
+    ]);
   }
+
+  public function testStoreTextUrlInDbAndFileOnDiskDesktop () 
+  {
+    Storage::fake('texts');
+    $user = factory(User::class)->create();
+    $rootFolder = $this->getRootFolder($user);
+    $fileInput = [
+      'type' => 'file',
+      'title' => 'Software testing',
+      'url' => 'https://en.wikipedia.org/wiki/Software_testing'
+    ];
+    $this->actingAs($user)->post(route('desk.store', ['currentFolderId' => $rootFolder->id]), $fileInput);
+    $this->assertDatabaseHas('files', [
+      'title' => 'Software testing',
+      'url' => 'https://en.wikipedia.org/wiki/Software_testing',
+      'parent_id' => $rootFolder->id
+    ]);
+    $newFile = File::where([
+      ['title', '=', 'Software testing'],
+      ['user_id', '=', $user->id]
+      ])->first();
+    Storage::disk('texts')->assertExists($newFile->id . '_' . $newFile->user_id . '.html');
+  }
+
+  public function testStoreTextUrlInDbAndFileOnDiskMobile () 
+  {
+    Storage::fake('texts');
+    $user = factory(User::class)->create();
+    $rootFolder = $this->getRootFolder($user);
+    $fileInput = [
+      'type' => 'file',
+      'title' => 'Software testing',
+      'url' => 'https://en.m.wikipedia.org/wiki/Software_testing'
+    ];
+    $this->actingAs($user)->post(route('desk.store', ['currentFolderId' => $rootFolder->id]), $fileInput);
+    $this->assertDatabaseHas('files', [
+      'title' => 'Software testing',
+      'url' => 'https://en.m.wikipedia.org/wiki/Software_testing',
+      'parent_id' => $rootFolder->id
+    ]);
+    $newFile = File::where([
+      ['title', '=', 'Software testing'],
+      ['user_id', '=', $user->id]
+    ])->first();
+    Storage::disk('texts')->assertExists($newFile->id . '_' . $newFile->user_id . '.html');
+  }
+  public function testDeleteFile()
+  {
+    $user = factory(User::class)->create();
+    $currentFolder = $this->getRootFolder($user);
+    $fileInput = [
+      'type' => 'file',
+      'title' => 'Software testing',
+      'url' => 'https://en.m.wikipedia.org/wiki/Software_testing',
+      'user_id' => $user->id,
+      'parent_id' => $currentFolder->id
+    ];
+    $file = File::create($fileInput);
+    $this->post(route('desk.delete', [
+      'elementType' => 'file',
+      'elementId' => $file->id
+    ]));
+    $this->assertDeleted('files', [
+      'id' => $file->id
+    ]);
+  }
+
+  public function testDeleteFolder ()
+  {
+    $user = factory(User::class)->create();
+    $currentFolder = $this->getRootFolder($user);
+    $folderInput = [
+      'type' => 'folder',
+      'title' => 'Testfolder',
+      'user_id' => $user->id,
+      'parent_id' => $currentFolder->id
+    ];
+    $folder = Folder::create($folderInput);
+    $this->post(route('desk.delete', [
+      'elementType' => 'folder',
+      'elementId' => $folder->id
+    ]));
+    $this->assertDeleted('folders', [
+      'id' => $folder->id
+    ]);
+  }
+
+  private function getRootFolder ($user)
+  {
+    $folderInput = [
+      'user_id' => $user->id,
+      'title' => 'Desk of' . $user->name,
+      'parent_id' => NULL
+    ];
+    return Folder::create($folderInput);
+  }
+}
+
